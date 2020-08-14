a43fb0e24aeaa9899ea2af4de6fa9abd77386966
Stefano Bossi <stefano.bossi@maersk.com>
2020-08-14 13:19:35 +0200

    add description

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,16 @@
 const sum = require('./index');
 
+
+// Your task
+//
+//
+//
+// Write a program that prints one line for each number from 1 to 100
+// For multiples of three print Fizz instead of the number
+// For the multiples of five print Buzz instead of the number
+// For numbers which are multiples of both three and five print FizzBuzz instead of the number
+
+
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
