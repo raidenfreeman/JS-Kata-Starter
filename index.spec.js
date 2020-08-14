2ff84228b351905aa723f889689016813cfd2fd8
dianatofan <diana_tofan@ymail.com>
2020-08-14 14:12:50 +0200

    Add output for numbers divisible with 3 and 5

--- index.spec.js
+++ index.spec.js
@@ -11,33 +11,41 @@ const sum = require('./index');
 // For numbers which are multiples of both three and five print FizzBuzz instead of the number
 
 function someFunction() {
-    let array = [];
-    for (let i = 1; i <= 100; i++) {
-        if(i%3 === 0) {
-            array.push("Fizz");
-        } else if(i%5 === 0){
-            array.push("Buzz");
-        } else {
-            array.push(i);
-        }
+  let array = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      array.push("Fizz");
+    } else if (i % 5 === 0) {
+      array.push("Buzz");
+    } else {
+      array.push(i);
     }
-    return array;
+  }
+  return array;
 }
 
 test('output 1', () => {
-    expect(someFunction()[0]).toEqual(1)
+  expect(someFunction()[0]).toEqual(1)
 })
 test('output 2', () => {
-    expect(someFunction()[1]).toEqual(2)
+  expect(someFunction()[1]).toEqual(2)
 })
 test('output Fizz for 3', () => {
-    expect(someFunction()[2]).toEqual("Fizz")
+  expect(someFunction()[2]).toEqual("Fizz")
 })
 test('output Fizz for 6', () => {
-    expect(someFunction()[5]).toEqual("Fizz")
+  expect(someFunction()[5]).toEqual("Fizz")
 })
 test('output Buzz for 5', () => {
-    expect(someFunction()[4]).toEqual("Buzz")
+  expect(someFunction()[4]).toEqual("Buzz")
+})
+test('output FizzBuzz for 15', () => {
+  expect(someFunction()[14]).toEqual("FizzBuzz")
+})
+test('output FizzBuzz for 30', () => {
+  expect(someFunction()[29]).toEqual("FizzBuzz")
 })
 
 
