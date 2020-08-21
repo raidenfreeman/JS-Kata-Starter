f620aa4b9ff66fb8484a73bd7aa73477f0f17da2
dianatofan <diana_tofan@ymail.com>
2020-08-21 13:37:00 +0200

    Support functionality for buying 2 books

---
+++ potter.spec.js
@@ -0,0 +1,26 @@
+function purchaseBook(number) {
+  if (number === 0) {
+    return 0;
+  } else if (number === 2) {
+    return 16;
+  }
+  return 8;
+}
+
+describe("harry potter books", () => {
+  it("true is true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("one book costs 8 EUR", () => {
+    expect(purchaseBook(1)).toEqual(8);
+  });
+
+  it('0 books cost 0 EUR', function () {
+    expect(purchaseBook(0)).toEqual(0);
+  });
+
+  it('2 books cost 16 EUR', function () {
+    expect(purchaseBook(2)).toEqual(16);
+  });
+});
